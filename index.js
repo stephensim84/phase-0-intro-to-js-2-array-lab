bf5bd37dd884bab88f0478a654ac9d0c3b20d6e7
Stephen Simmons <stephensimmons@stephens-MacBook-Pro.local>
2022-03-15 08:43:40 -0700

    commit tree 0da4b2299895cf7f8adf0ae21841379ee2178beb

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Write your solution here!
+let cats = ['Milo', 'Otis', 'Garfield'];
+ function destructivelyAppendCat(name){
+return cats.push(name)
+ };
+
+ function destructivelyPrependCat(name){
+return cats.unshift(name)
+ }
+ 
+ function destructivelyRemoveLastCat(){
+    
+  return cats.pop()
+ }
+
+ function destructivelyRemoveFirstCat(name) {
+     return cats.shift()
+ }
+ function appendCat(name){
+    let name_1= [...cats,name];
+    return name_1
+  }
+  function appendCat(name){
+    let name_1= [...cats,name];
+    return name_1;
+  }
+  
+  function prependCat(name){
+    let name_2=[name, ...cats];
+    return name_2
+  }
+  
+  function removeLastCat(){
+    let caats= cats.slice(0,cats.length-1);
+    return caats;
+  }
+  
+  function removeFirstCat(){
+    let catts= cats.slice(1)
+    return catts
+  }
